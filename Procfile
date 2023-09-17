f58e582514bff164b8d8cea16c8d23588ec8c115
syrfmy <syariefmulyadi24@gmail.com>
2023-09-17 13:12:48 +0700

    first deployment

---
+++ Procfile
@@ -0,0 +1,2 @@
+release: django-admin migrate --noinput
+web: gunicorn my_book_collection.wsgi
--- main/views.py
+++ main/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from main.forms import ProductForm
 from main.models import Product
 from django.urls import reverse
+from django.core import serializers
 
 def show_main(request):
     context = {
@@ -23,4 +24,24 @@ def create_product(request):
     context = {'form': form}
     return render(request, "create_product.html", context)
 
-# Create your views here.
+def show_book(request):
+    context = {
+        'products':Product.objects.all()
+    }
+    return render(request, "show_book.html", context)
+
+def show_xml(request):
+    data = Product.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type= "application/xml")
+
+def show_json(request):
+    data = Product.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_xml_by_id(request, id):
+    data = Product.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json_by_id(request, id):
+    data = Product.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
