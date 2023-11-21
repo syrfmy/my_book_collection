5febf1298a0a1594e03a2e3760f0296efaa7418d
syrfmy <syariefmulyadi24@gmail.com>
2023-11-22 06:05:24 +0700

    deploy

--- my_book_collection/settings.py
+++ my_book_collection/settings.py
@@ -81,7 +81,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'my_book_collection.wsgi.application'
+WSGI_APPLICATION = 'my_book_collection.wsgi.app'
 
 
 # Database
@@ -137,8 +137,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -151,5 +151,6 @@ CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SAMESITE = 'None'
 SESSION_COOKIE_SAMESITE = 'None'
+
 
 
